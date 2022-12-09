75394e871c3ab044c7209ce9876814225325ce47
vinodkadam02 <vinod.kadam@theelixrlabs.com>
2022-12-09 13:38:33 +0530

    Made inputs as command line arguments. Renamed Method to processFile.

--- src/PerformWordSearchOperation.java
+++ src/PerformWordSearchOperation.java
@@ -1,19 +1,20 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
-/**
+/*
  * Creating a class for Spawn thread
  * Declaring the filepath variable with private access modifier
  * And creating a constructor for the filepath to access it from the other class
  */
 public class PerformWordSearchOperation extends Thread {
     private final String filepath;
+    private final String keyWordToSearch;
 
-    public PerformWordSearchOperation(String filepath) {
+    public PerformWordSearchOperation(String filepath, String keyWordToSearch) {
         this.filepath = filepath;
+        this.keyWordToSearch = keyWordToSearch;
     }
 
     /*
@@ -23,9 +24,9 @@ public class PerformWordSearchOperation extends Thread {
     Reading the keyword to check in the file and Storing it in the variable keyWordToSearch
     Checking if the Keyword present in the file or not.
      */
+
     public void run() {
         int wordOccurrence = 0;
-        Scanner scanner = new Scanner(System.in);
         String fileContent;
         try {
             fileContent = Files.readString(Path.of(filepath));
@@ -37,8 +38,6 @@ public class PerformWordSearchOperation extends Thread {
             throw new RuntimeException(e);
         }
         fileContent = fileContent.replaceAll("[^a-zA-Z0-9@-]", " ");
-        System.out.println("Enter the word to search");
-        String keyWordToSearch = scanner.next();
         StringTokenizer fileContentTokenizer = new StringTokenizer(fileContent);
         while (fileContentTokenizer.hasMoreTokens()) {
             if (keyWordToSearch.equalsIgnoreCase(fileContentTokenizer.nextToken())) {
--- src/WordSearch.java
+++ src/WordSearch.java
@@ -5,22 +5,26 @@ import java.io.File;
  * calling the filePath method for searching the file in system
  */
 public class WordSearch {
-    public static void main(String[] args) throws Exception {
-        String filepath = args[0];
-        WordSearch wordsearch = new WordSearch();
-        wordsearch.filePath(filepath);
+    public static void main(String[] args) {
+        if (args.length == 2) {
+            String filepath = args[0];
+            String keyWordToSearch = args[1];
+            WordSearch wordsearch = new WordSearch();
+            wordsearch.processFile(filepath, keyWordToSearch);
+        } else {
+            System.out.println("Please pass the Arguments");
+        }
     }
 
     /*
     Assigning the file path to File and checking file is Present in the system or not
     */
-    public void filePath(String filepath) throws Exception {
+    public void processFile(String filepath, String keyWordToSearch) {
         System.out.println("Searching File...");
-        Thread.sleep(500);
         File file = new File(filepath);
         if (file.exists()) {
             System.out.println("The File exists");
-            PerformWordSearchOperation performOperation = new PerformWordSearchOperation(filepath);
+            PerformWordSearchOperation performOperation = new PerformWordSearchOperation(filepath, keyWordToSearch);
             performOperation.start();
         } else {
             System.out.println("File Does Not Exists In the System");
